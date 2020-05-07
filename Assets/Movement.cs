407bf29ea07e3d1c653422023b6f631b286d3d19
Unknown635 <49661384+Unknown635@users.noreply.github.com>
2020-05-07 15:33:26 -0500

    game Finished!
    Added enemies, it is now an aim trainer game! We changed the original view a lot, but this is the final result!

---
+++ Assets/Movement.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Movement : MonoBehaviour
+{
+    [SerializeField]
+    Transform[] wayPoints;
+    int currentWayPoint = 0;
+    Rigidbody RidgibB;
+    [SerializeField]
+    float moveSpeed = 5;
+    // Start is called before the first frame update
+    void Start()
+    {
+        RidgibB = GetComponent<Rigidbody>();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        movement();
+    }
+    void movement()
+    {
+        if (Vector3.Distance(transform.position, wayPoints[currentWayPoint].position) < .25f)
+            {
+            currentWayPoint += 1;
+            currentWayPoint = currentWayPoint % wayPoints.Length;
+        }   
+        Vector3 _dir = (wayPoints[currentWayPoint].position - transform.position).normalized;
+        RidgibB.MovePosition(transform.position + _dir * moveSpeed * Time.deltaTime);
+    }
+}
